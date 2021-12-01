6ac8c728e186c4b08e03cd3638b2bc3f24d880f1
yamanoha-hab <r12m097@yahoo.co.jp>
2021-12-01 11:28:18 +0900

    お知らせタブ設定

--- js/script.js
+++ js/script.js
@@ -45,25 +45,19 @@ $(function() {
     $('.news__tab li').removeClass('tab-active');
     // クリックしたタブにボーダーをつける
     $(this).addClass('tab-active');
-    
-    // 全てのコンテンツ非表示
-    // $(".news__contents__item").hide();
 
     // クリックした要素のIDを取得
     var IDname = $(this).attr('id');
     // 確認
-    console.log(IDname);
-    
+    // console.log(IDname);
 
-    // $('.news__contents a[href != "#"]').hide();
+    // 全てのタグを削除
     $('.news__contents a').hide();
 
-    // $('.news__contents a[href = "#others"]').show();
-
-    // IDnameとnews__contents aのhrefがあっていたら
-    if(IDname = 'date-info') {
+    // IDnameとnews__contents aのhrefがあっていたら表示
+    if(IDname == 'date-info') {
       $('.news__contents a[href = "#date-info"]').show();
-    } else if(IDname = 'others') {
+    } else if(IDname == 'others') {
       $('.news__contents a[href = "#others"]').show();
     } else {
       $(".news__contents a").show();
